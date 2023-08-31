4b8092d893c757f69e291961331f705c961bad07
Prashant <Prashant@DESKTOP-LRS8VJJ>
2023-08-31 11:17:19 +0530

    [sammati]Class1 created

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Prashant
+ *
+ */
+module Github1 {
+}
---
+++ src/p1/Class1.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class Class1 {
+
+	public static void main(String[] args)
+	{
+       System.out.println("Hi");
+	}
+
+}
